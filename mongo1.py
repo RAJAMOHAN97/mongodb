b3c268b1cae376e3c994a8670560ff5c13444e39
RAJAMOHAN97 <dunnuthalarajamohanreddy@gmail.com>
2022-11-29 22:45:48 +0530

    THIS IS MY FIRST PROGRAM TO PUSH

---
+++ mongo1.py
@@ -0,0 +1,14 @@
+import pymongo
+client=pymongo.MongoClient("mongodb+srv://rajamohanreddy:Raja3c4@cluster1.ipjaewy.mongodb.net/?retryWrites=true&w=majority")
+db=client.test
+print(db)
+
+d= {
+    "name" :"rajamohan",
+    "edu" : "btech",
+    "location":"kadapa"
+}
+
+db1= client['mongo1']
+coll = db1['test']
+coll.insert_one(d )
